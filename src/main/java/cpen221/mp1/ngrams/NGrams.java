0c021a39db214266e87ea7d8326f7e633d80bc86
Felix Ma <felixma.home@gmail.com>
2022-09-30 13:37:35 -0700

    getNGrams logic

--- src/main/java/cpen221/mp1/ngrams/NGrams.java
+++ src/main/java/cpen221/mp1/ngrams/NGrams.java
@@ -1,9 +1,12 @@
 package cpen221.mp1.ngrams;
 
+import java.util.*;
 import java.util.List;
 import java.util.Map;
 
 public class NGrams {
+
+    String[] line;
 
     /**
      * Create an NGrams object
@@ -12,7 +15,7 @@ public class NGrams {
      *             is not null and is not empty.
      */
     public NGrams(String[] text) {
-
+        this.line = text;
     }
 
     /**
@@ -36,6 +39,39 @@ public class NGrams {
      * with the i-th entry being all the (i+1)-grams and their counts
      */
     public List<Map<String, Long>> getAllNGrams() {
-        return null; // TODO: Implement this method
+
+        //Initialize List of Maps
+        List<Map<String, Long>> listOfGrams = new ArrayList<Map<String, Long>>();
+
+        //Iterate 1 through to longest possible gram, add map to list
+        for(int glength = 1; glength <= this.line.length; glength++) {
+
+            listOfGrams.add(new HashMap<String, Long>());
+
+            //Iterate through line with each word as starting word in gram
+            for(int j = 0; j <= this.line.length - glength; j++) {
+
+                //Using StringBuilder, construct current String of glength words
+                StringBuilder current = new StringBuilder();
+
+                for (int k = j; k < j + glength; k++) {
+                    current.append(this.line[k]);
+                    if (k != j + glength - 1) {
+                        current.append(" ");
+                    }
+                }
+                String currentStr = current.toString();
+
+                //Check whether the current String is in the current map
+                if (listOfGrams.get(glength - 1).containsKey(currentStr)) {
+                    Long count = listOfGrams.get(glength - 1).get(currentStr);
+                    listOfGrams.get(glength - 1).put(currentStr, ++count);
+                } else {
+                    listOfGrams.get(glength - 1).put(currentStr, 1L);
+                }
+            }
+        }
+
+        return listOfGrams;
     }
 }

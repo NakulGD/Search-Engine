e154bab4738e79310744258bc60b3c5b06044c2b
Anu <anumithrap@gmail.com>
2022-10-12 19:02:15 -0700

    imported stuff for the test files

--- src/test/java/autocompletion/Task3PublicTests.java
+++ src/test/java/autocompletion/Task3PublicTests.java
@@ -1,4 +1,12 @@
 package autocompletion;
 
+import cpen221.mp1.autocompletion.AutoCompletor;
+import cpen221.mp1.cities.DataAnalyzer;
+import cpen221.mp1.searchterm.SearchTerm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class Task3PublicTests {
+
 }
